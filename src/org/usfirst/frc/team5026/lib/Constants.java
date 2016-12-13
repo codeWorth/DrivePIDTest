9524eb3d3e588b203f85f1ed722e876cb5341bc4
Drewpew <agcummings11@gmail.com>
2016-12-13 08:52:15 -0800

    Update Constants.java

--- src/org/usfirst/frc/team5026/lib/Constants.java
+++ src/org/usfirst/frc/team5026/lib/Constants.java
@@ -2,10 +2,13 @@ package org.usfirst.frc.team5026.lib;
 
 public class Constants {
 	
-	public static final double DRIVEBASE_P = 0;
-	public static final double DRIVEBASE_I = 0;
-	public static final double DRIVEBASE_D = 0;
-	public static final double DRIVEBASE_F = 0;
+	public static final double DRIVE_SPEED_P = 0;
+	public static final double DRIVE_SPEED_I = 0;
+	public static final double DRIVE_SPEED_D = 0;
+
+	public static final double DRIVE_TURN_P = 0;
+	public static final double DRIVE_TURN_I = 0;
+	public static final double DRIVE_TURN_D = 0;
 	
 	public static final double DRIVEBASE_MAXSPEED = 1000;
 	
